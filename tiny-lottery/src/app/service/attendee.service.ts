0c01c60b5b44613d0162c712b29310c00c408ba4
Xiaodi Yan <yan_xiaodi@hotmail.com>
2019-02-23 16:04:00 +1300

    Handle the error.

--- tiny-lottery/src/app/service/attendee.service.ts
+++ tiny-lottery/src/app/service/attendee.service.ts
@@ -1,8 +1,8 @@
 import { Attendee } from './../model/attendee';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +27,20 @@ export class AttendeeService {
           attendees.push(attendee);
         });
         return attendees;
-      }));
+      }), catchError(this.handleError<Array<Attendee>>('getAttendees', [])));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
